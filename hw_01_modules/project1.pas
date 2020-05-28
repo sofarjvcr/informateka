7bfeb9563949f290c25245e66bb5c52aad1402bd
Михаил <mihail.latyshov2@mail.ru>
2020-05-28 23:35:19 +0300

    hw_01_modules_verification

--- hw_01_modules/project1.pas
+++ hw_01_modules/project1.pas
@@ -5,11 +5,12 @@ uses  geometry;
 var
   T: Triangle;
   L: Line;
-
+  R: TRectangle;
 begin
 
   T:= CreateTriangle();
   L:= CreateLine() ;
+  R:= CreateRectangle();
 
   WriteLn('AB = ', GetDistance(T.A, T.B):0:6);
   WriteLn('BC = ', GetDistance(T.B, T.C):0:6);
@@ -19,7 +20,11 @@ begin
   WriteLn('Peremetr = ', GetTrPer(T):0:6);
   WriteLn('Square = ', GetSquare(T):0:6);
 
-  WriteLn('IsCrossed = ', CrossTriAndLine(T, L));
+  WriteLn('Is Crossed = ', CrossTriAndLine(T, L));
+
+
+
+  Writeln('Is Bigger = ', GetBigger(T, R));
 
   Readln();
 end.
